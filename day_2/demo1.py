a9bf97b05a2cb41f44f17e481b5255b8d4fb4d18
Osama Abou Hajar <smsm.sy@hotmail.com>
2021-05-23 20:23:41 +0100

    Commit After Day 2

---
+++ day_2/demo1.py
@@ -0,0 +1,17 @@
+age = 28
+name = "osama" 
+adult = True  
+
+# print("test")
+print(name)
+print(adult)
+
+## Operation
+age = 28 + 10
+
+# 11  -> 2 + 2+ 2+ 2+ 2 + 1
+# 11 % 2 >>>>> 1
+# 5 % 10 >>> 5 % 10 = 5
+# 10  + 5 = 15
+
+
---
+++ day_2/exe1_var.py
@@ -0,0 +1,19 @@
+name = "Osama"
+age = 28
+email = "oabouhajar@hotmail.com"
+is_student = True
+blood_group = 'A+'
+
+
+# user input for today var
+today = input("What is Today? ")
+print("Hello:" , name)
+print("Happy", today)
+
+
+age = input("Enter your age please! ")
+
+# Print user input
+print("Enjoy your",age)
+print("Your age input result is: ",type(age))
+
